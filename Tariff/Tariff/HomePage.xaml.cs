af98e8b95df4cc2446640ea351d5f4f3123b7587
Andrey Gusev <galligalli16@gmail.com>
2022-11-27 12:19:19 +0300

    Пробный заход. Добавил передачу показаний и проверку на то, что они являются числами

--- Tariff/Tariff/HomePage.xaml.cs
+++ Tariff/Tariff/HomePage.xaml.cs
@@ -16,5 +16,27 @@ namespace Tariff
         {
             InitializeComponent();
         }
+
+        double hot;
+        double cold;
+        double gas;
+        double electro;
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(Hot_Water.Text, out hot) && double.TryParse(Cold_Water.Text, out cold) && double.TryParse(Gas_numbers.Text, out gas) && double.TryParse(Electro_numbers.Text, out electro))
+            {
+                DisplayAlert("Успешно", "Показания переданы", "Ok");
+                Navigation.PushModalAsync(new HomePage());
+            }
+            else
+            {
+                DisplayAlert("Oops...", "Показания должны быть числами", "OK");
+            }
+
+        }
+
+
+
     }
 }
