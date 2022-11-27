daaba239247e8fd56fab422a2af5da1e01819279
guseinov <gusei@NAMIG>
2022-11-27 17:33:05 +0300

    1. Уменьшил размер домашней страницы 2. Добавил макет для истории передачи показаний 3. Добавил в манифист код для передачи данных по почте 4. Функция для передачи по почте работает некорректно

--- Tariff/Tariff/HomePage.xaml.cs
+++ Tariff/Tariff/HomePage.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Xamarin.Essentials;
+
 namespace Tariff
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -22,21 +24,40 @@ namespace Tariff
         double gas;
         double electro;
 
-        private void Button_Clicked(object sender, EventArgs e)
+        async private void Button_Clicked(object sender, EventArgs e)
         {
             if (double.TryParse(Hot_Water.Text, out hot) && double.TryParse(Cold_Water.Text, out cold) && double.TryParse(Gas_numbers.Text, out gas) && double.TryParse(Electro_numbers.Text, out electro))
             {
-                DisplayAlert("Успешно", "Показания переданы", "Ok");
-                Navigation.PushModalAsync(new HomePage());
-            }
-            else
-            {
-                DisplayAlert("Oops...", "Показания должны быть числами", "OK");
+                await DisplayAlert("Успешно", "Показания успешно переданы", "OK");
+                // await Email.ComposeAsync("guseinovnamig41@gmail.com", "", "Привет");
             }
 
         }
 
+        public async Task SendEmail(string subject, string body, List<string> recipients)
+        {
+            try
+            {
+                var message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                };
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException fbsEx)
+            {
+                // Email is not supported on this device
+                await DisplayAlert("Ошибка", "Данные не переданы", "OK");
+            }
+            catch (Exception ex)
+            {
+                // Some other exception occurred
 
+                await DisplayAlert("Ошибка", "Данные не переданы", "OK");
+            }
+        }
 
     }
 }
